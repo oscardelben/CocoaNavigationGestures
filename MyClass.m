278cde4127e018c404e26180afdbb88773331096
Matt Shepherd <matt.c.shepherd@gmail.com>
2014-11-06 20:48:10 -0800

    Updated to support Yosemite
    This updates the code to work on yosemite, it provides no backward
    support, you can implement that by looking at the older version.

--- MyClass.m
+++ MyClass.m
@@ -1,93 +1,59 @@
 #import "MyClass.h"
 
-#define kSwipeMinimumLength 0.3
+#define kSwipeMinimumLength 0.2
 
 @implementation MyClass
 
-@synthesize twoFingersTouches;
-
-// Three fingers gesture, Lion (if enabled) and Leopard
-- (void)swipeWithEvent:(NSEvent *)event {
-    CGFloat x = [event deltaX];
-    //CGFloat y = [event deltaY];
-    
-    if (x != 0) {
-		(x > 0) ? [self goBack] : [self goForward];
-	}
-}
-
-
--(BOOL)recognizeTwoFingerGestures
-{
-    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-    return [defaults boolForKey:@"AppleEnableSwipeNavigateWithScrolls"];
-}
-
-- (void)beginGestureWithEvent:(NSEvent *)event
-{
-    if (![self recognizeTwoFingerGestures])
-        return;
-    
-    NSSet *touches = [event touchesMatchingPhase:NSTouchPhaseAny inView:nil];
-    
-    self.twoFingersTouches = [[NSMutableDictionary alloc] init];
-    
-    for (NSTouch *touch in touches) {
-        [twoFingersTouches setObject:touch forKey:touch.identity];
+- (void)touchesBeganWithEvent:(NSEvent *)event{
+    if(event.type == NSEventTypeGesture){
+        NSSet *touches = [event touchesMatchingPhase:NSTouchPhaseAny inView:self];
+        if(touches.count == 2){
+            self.twoFingersTouches = [[NSMutableDictionary alloc] init];
+            
+            for (NSTouch *touch in touches) {
+                [self.twoFingersTouches setObject:touch forKey:touch.identity];
+            }
+        }
     }
 }
 
-- (void)endGestureWithEvent:(NSEvent *)event
-{
-    if (!twoFingersTouches) return;
-    
-    NSSet *touches = [event touchesMatchingPhase:NSTouchPhaseAny inView:nil];
-    
-    // release twoFingersTouches early
-    NSMutableDictionary *beginTouches = [twoFingersTouches copy];
-    self.twoFingersTouches = nil;
-    
-    NSMutableArray *magnitudes = [[NSMutableArray alloc] init];
-    
-    for (NSTouch *touch in touches) 
-    {
-        NSTouch *beginTouch = [beginTouches objectForKey:touch.identity];
+
+- (void)touchesMovedWithEvent:(NSEvent*)event {
+    NSSet *touches = [event touchesMatchingPhase:NSTouchPhaseEnded inView:self];
+    if(touches.count > 0){
+        NSMutableDictionary *beginTouches = [self.twoFingersTouches copy];
+        self.twoFingersTouches = nil;
         
-        if (!beginTouch) continue;
+        NSMutableArray *magnitudes = [[NSMutableArray alloc] init];
         
-        float magnitude = touch.normalizedPosition.x - beginTouch.normalizedPosition.x;
-        [magnitudes addObject:[NSNumber numberWithFloat:magnitude]];
+        for (NSTouch *touch in touches)
+        {
+            NSTouch *beginTouch = [beginTouches objectForKey:touch.identity];
+            
+            if (!beginTouch) continue;
+            
+            float magnitude = touch.normalizedPosition.x - beginTouch.normalizedPosition.x;
+            [magnitudes addObject:[NSNumber numberWithFloat:magnitude]];
+        }
+        
+        float sum = 0;
+        
+        for (NSNumber *magnitude in magnitudes)
+            sum += [magnitude floatValue];
+        
+        // See if absolute sum is long enough to be considered a complete gesture
+        float absoluteSum = fabsf(sum);
+        
+        if (absoluteSum < kSwipeMinimumLength) return;
+        
+        // Handle the actual swipe
+        // This might need to be > (i am using flipped coordinates)
+        if (sum > 0){
+            NSLog(@"go back");
+        }else{
+            NSLog(@"go forward");
+        }
     }
-    
-    // Need at least two points
-    if ([magnitudes count] < 2) return;
-    
-    float sum = 0;
-    
-    for (NSNumber *magnitude in magnitudes)
-        sum += [magnitude floatValue];
-    
-    // Handle natural direction in Lion
-    BOOL naturalDirectionEnabled = [[[NSUserDefaults standardUserDefaults] valueForKey:@"com.apple.swipescrolldirection"] boolValue];
-    
-    if (naturalDirectionEnabled)
-        sum *= -1;
-    
-    // See if absolute sum is long enough to be considered a complete gesture
-    float absoluteSum = fabsf(sum);
-
-    if (absoluteSum < kSwipeMinimumLength) return;
-
-    // Handle the actual swipe
-    if (sum > 0) 
-    {
-        [self goForward];
-    } else
-    {
-        [self goBack];
-    }
-    
-    
 }
 
 @end
